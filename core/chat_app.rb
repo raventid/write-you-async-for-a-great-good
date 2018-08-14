269e8e0fcd67daacefc8ccf2754cc8c5e7b8539c
Julian Kulesh <julian.kulesh@kupibilet.ru>
2018-08-15 00:40:04 +0300

    final version of learning purpose Stream realisation

--- core/chat_app.rb
+++ core/chat_app.rb
@@ -11,62 +11,54 @@ class Stream
     @writebuffer = ""
   end
 
-  # Let's avoid EventEmitter for now?
-  # We already have it, so should we just avoid term?
-  # Well, ok let's avoid it for now and call this
-  # just callbacks, we have to introduce callbacks first.
-  ## EVENTEMITTER START
   def _callbacks
     @_callbacks ||= Hash.new { |h, k| h[k] = [] }
   end
 
-  def on(type, &blk)
-    _callbacks[type] << blk
+  def register_callback_for(type, &callback)
+    _callbacks[type] << callback
     self
   end
 
-  def emit(type, *args)
-    _callbacks[type].each do |blk|
-      blk.call(*args)
+  def run_registered_callbacks_for(type, *args)
+    _callbacks[type].each do |callback|
+      callback.call(*args)
     end
-  end
-  ## EVENTEMITTER END
-
-  def to_io
-    @socket
   end
 
   def <<(chunk)
-    # just append this to buffer, handle_write
-    # will make an actual write.
+    # Just append chunk to buffer,
+    # `handle_write` will make an actual write.
     @writebuffer << chunk
   end
 
   def handle_read
     chunk = @socket.read_nonblock(4096)
-    # this need to be improved, emit(:close) just removes
-    # clients from sever and EV lists, but does not close
-    # socket, so client wait for some server reaction and
-    # gets nothing in response.
-    # emit(:close) if chunk.include?(":q")
-    emit(:data, chunk)
+    run_registered_callbacks_for(:data, chunk)
   rescue IO::WaitReadable
-    # Oops, IO wasn't readable, someone made a mistake
+    # Oops, IO wasn't readable for some reason
+    # let's ignore read and wait for the next time
   rescue EOFError, Errno::ECONNRESET
-    # IO was closed
-    emit(:close)
+    # IO was closed by client
+    run_registered_callbacks_for(:close)
   end
 
   def handle_write
     return if @writebuffer.empty?
+    # write_nonblock will tell us how many bytes
+    # were successfuly written.
     length = @socket.write_nonblock(@writebuffer)
     # Remove the data which that was successfuly written
     @writebuffer.slice!(0, length)
-    # Emit `drain` event if there's nothing more to write
-    emit(:drain) if @writebuffer.empty?
+    # Run callbacks for `drain` event if there's nothing more to write
+    run_registered_callbacks_for(:drain) if @writebuffer.empty?
   rescue IO::WaitWritable
   rescue EOFError, Errno::ECONNRESET
-    emit(:close)
+    run_registered_callbacks_for(:close)
+  end
+
+  def to_io
+    @socket
   end
 end
 
