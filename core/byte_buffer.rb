daab8194ed5757808147e5f11b0753dc77691397
Julian Kulesh <juliankul@gmail.com>
2018-08-22 00:11:46 +0300

    some code addition for byte_buffer

--- core/byte_buffer.rb
+++ core/byte_buffer.rb
@@ -1,4 +1,14 @@
 module Core
   class ByteBuffer
+    # ByteBuffer is just a very big string with Binary Encoding.
+    # We need to track position inside this string, limit and capacity.
+    #
+    #
+    #
+    attr_reader :position, :limit, :capacity
+
+    def intitialize(capacity)
+    end
+
   end
 end
