9a999970c85b2b312aaf3cd62ec015bb985a4fa5
Julian Kulesh <juliankul@gmail.com>
2018-08-25 22:47:20 +0300

    core -> foundation, byte_buffer finished

--- core/byte_buffer.rb
+++ core/byte_buffer.rb
@@ -1,4 +1,4 @@
-module Core
+module Foundation
   class ByteBuffer
     # ByteBuffer is just a very big string with Binary Encoding.
     # We need to track position inside this string, limit and capacity.
@@ -7,9 +7,12 @@ module Core
     #
     attr_reader :position, :limit, :capacity
 
-    def intitialize(capacity)
+    def initialize(capacity)
       @capacity = capacity
       @buffer = ("\0" * @capacity).force_encoding(Encoding::BINARY)
     end
   end
 end
+
+# That's kinda like unit tests.
+buffer = Foundation::ByteBuffer.new(16384)
