828031a941b9f3a2b706582bcdf33aca7a5a5405
Julian Kulesh <juliankul@gmail.com>
2018-08-27 21:48:22 +0300

    << for byte_buffer

--- core/byte_buffer.rb
+++ core/byte_buffer.rb
@@ -11,8 +11,31 @@ module Foundation
       @capacity = capacity
       @buffer = ("\0" * @capacity).force_encoding(Encoding::BINARY)
     end
+
+    def <<(str)
+      raise "expected String, got #{str.class}" unless str.respond_to?(:to_str)
+      str = str.to_str
+
+      # Available space in is easy to calculate,
+      # it is maximal cursor position minus current cursor position
+      available_space = @limit - @position
+
+      raise "buffer overflow" if str.length > available_space
+
+      # We have to select the part of byte_buffer
+      # from current cursor position to str.length
+      @buffer[@position...str.length] = str
+
+      # Here we shift position by the length of string
+      @position += str.length
+      self
+    end
   end
 end
 
 # That's kinda like unit tests.
-buffer = Foundation::ByteBuffer.new(16384)
+
+# Let's create our byte buffer
+buffer = Foundation::ByteBuffer.new(100)
+
+buffer << "Hello"
